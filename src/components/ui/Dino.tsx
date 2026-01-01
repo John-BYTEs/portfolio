e3a9fcaa3d8d082ce75adcc9881777d47bfd99db
John-BYTEs <wolfejohndel@gmail.com>
2026-01-01 14:44:54 +0800

    Patched the useless duck animation

--- src/components/ui/Dino.tsx
+++ src/components/ui/Dino.tsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from 'react';
 import './Dino.css';
 
 const Dino = () => {
-  const [ducking, setDucking] = useState(false);
-
-  // Autoplay duck loop only
-  useEffect(() => {
-    const duckTimer = setInterval(() => {
-      setDucking(d => !d);
-    }, 2000);
-
-    return () => clearInterval(duckTimer);
-  }, []);
-
   return (
     <div className="dino-wrapper">
-      <div className={`dino-sprite ${ducking ? 'duck' : 'run'}`} />
+      <div className={`dino-sprite run`} />
     </div>
   );
 };
