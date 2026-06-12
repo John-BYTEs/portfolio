a2035d0d77af406b849cd56ca58cfc0760e16a9d
John-BYTEs <wolfejohndel@gmail.com>
2026-06-12 18:23:07 +0800

    feat: added dotted grid background

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App dotted-grid">
       <Header />
       <main>
         <section id="home">
